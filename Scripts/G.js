c4c8a8da26dc46ac838e2b41cb53a649fb4dbd7c
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-17 10:51:35 +0800

    Update G.js

--- Scripts/G.js
+++ Scripts/G.js
@@ -1,4 +1,4 @@
-// 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user-timeline.js
+// 引用地址：https://raw.githubusercontent.com/suiyuran/stash/main/scripts/fix-vvebo-user.js
 let url = $request.url;
 let hasUid = (url) => url.includes("uid");
 let getUid = (url) => (hasUid(url) ? url.match(/uid=(\d+)/)[1] : undefined);
@@ -13,10 +13,10 @@ if (url.includes("users/show")) {
 } else if (url.includes("profile/statuses/tab")) {
   let data = JSON.parse($response.body);
   let statuses = data.cards
-    .map((card) => (card.card_group ? card.card_group : card))
-    .flat()
-    .filter((card) => card.card_type === 9)
-    .map((card) => card.mblog);
+    。map((card) => (card.card_group ? card.card_group : card))
+    。flat()
+    。filter((card) => card.card_type === 9)
+    。map((card) => card.mblog);
   let sinceId = data.cardlistInfo.since_id;
   $done({ body: JSON.stringify({ statuses, since_id: sinceId, total_number: 100 }) });
 } else {
