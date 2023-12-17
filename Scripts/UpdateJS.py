64ce9cb13ff081ca9767074011a34269e63a2fa3
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-17 10:37:32 +0800

    Update UpdateJS.py

--- Scripts/UpdateJS.py
+++ Scripts/UpdateJS.py
@@ -2,7 +2,6 @@ import os
 import requests
 import re
 from requests.exceptions import RequestException
-from datetime import datetime, timedelta, timezone
 
 def download_file_from_comment(file_path):
     try:
@@ -18,13 +17,17 @@ def download_file_from_comment(file_path):
                 response = requests.get(download_url)
                 response.raise_for_status()
 
-                filename = os.path.join("Scripts", f"{os.path.splitext(os.path.basename(file_path))[0]}.js")
+                # 提取在“Scripts”目录内的相对路径
+                relative_path = os.path.relpath(file_path, 'Scripts')
 
-                with open(filename, 'wb') as new_file:
+                # 构建在“Scripts”目录内的新文件路径
+                new_file_path = os.path.join("Scripts", f"{os.path.splitext(relative_path)[0]}.js")
+
+                with open(new_file_path, 'wb') as new_file:
                     new_file.write(f"// 引用地址：{download_url}\n".encode('utf-8'))
                     new_file.write(response.content)
 
-                print(f"文件 {filename} 下载成功！")
+                print(f"文件 {new_file_path} 下载成功！")
             else:
                 print(f"在文件 {file_path} 中未找到引用地址")
     except Exception as e:
@@ -32,7 +35,6 @@ def download_file_from_comment(file_path):
 
 # 文件夹路径
 folder_path = "Scripts"
-
 
 # 遍历文件夹及其子文件夹内的所有文件
 for root, dirs, files in os.walk(folder_path):
