80a78324ad547613030729b69109594f8dbe1531
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-26 23:38:36 +0800

    Update rule.py

--- MergeScript/rule.py
+++ MergeScript/rule.py
@@ -5,9 +5,7 @@ import requests
 from concurrent.futures import ThreadPoolExecutor
 import subprocess
 
-
 def process_file(file_path):
-    # 使用 Sed 和 Awk 命令进行处理
     subprocess.run(['sed', '-i', 's/, /,/g', file_path])
     subprocess.run(['awk', '{sub("host-wildcard.*$", ""); print}', file_path], stdout=open('tmpfile', 'w'))
     subprocess.run(['mv', 'tmpfile', file_path])
@@ -21,48 +19,8 @@ def process_file(file_path):
     subprocess.run(['sed', '-i', '/IP-CIDR/ {/no-resolve/! s/$/,no-resolve/}; /IP-CIDR6/ {/no-resolve/! s/$/,no-resolve/}', file_path])
     subprocess.run(['sed', '-i', 's/;/# /g', file_path])
 
-RULES = {
-    "CorrectionRule": {
-        "fenliuxiuzheng": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliuxiuzheng.list",
-        "MyCorrectionRule": "https://raw.githubusercontent.com/GiveYou32Likes/Profile/main/QuantumultX/Rule/CorrectionRule.list",
-    },
-    "RejectRule": {
-        "fenliu": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliu.list",
-        "MyBlockAds": "https://raw.githubusercontent.com/limbopro/Adblock4limbo/main/Surge/rule/Adblock4limbo_surge.list",
-        "MyRejectRule": "https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/Reject.list",
-        "Block": "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list"
-    },
-    "StreamingRule": {
-        "Netflix": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/PayPal.list",
-        "Disney": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/OpenAI.list",
-        "YouTube": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/TikTok.list",
-        "Spotify": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/ProxyMedia.list",
-        "AppleTV": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Twitter.list",
-        "Netfli": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Telegram.list",
-        "Disne": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Game.list",
-        "YouTub": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Microsoft.list",
-        "Spotif": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Google.list",
-        "AppleT": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/AppleProxy.list",
-        "Spoti": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Apple.list",
-        "Apple": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Proxy.list",
-        "Appl": "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/BilibiliHMT.list",
-        "App": "https://raw.githubusercontent.com/ConnersHua/RuleGo/master/Surge/Ruleset/Extra/Streaming/Video/bilibili.list",
-        
-    }
-}
-
-HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
-TYPES = "Surge"
-file_paths = [TYPES + "/" + rule for rule in RULES.keys()]
-
-def download_and_save_file(url, target_path):
-    response = requests.get(url, headers=HEADER)
-    if response.status_code == 200:
-        with open(target_path, "wb") as f:
-            f.write(response.content)
-        time.sleep(1)
-
-def apply_replacements(line):
+# 在 apply_replacements 函数中添加参数 replacements
+def apply_replacements(line, replacements):
     for pattern, replacement in replacements:
         line = re.sub(pattern, replacement, line)
     return line
@@ -85,8 +43,15 @@ def load_files(rules, folder):
         with open(path, 'r', encoding='utf8') as file:
             lines = [line.strip() for line in file.readlines()]
 
+        # 在这里定义替换规则
+        replacements = [
+            # 你的替换规则格式应该是 (pattern, replacement)
+            (r'原始模式', r'替换后的内容'),
+            # 添加其他替换规则
+        ]
+
         # 应用替换规则
-        modified_lines = [apply_replacements(line) for line in lines]
+        modified_lines = [apply_replacements(line, replacements) for line in lines]
 
         # 保存修改后的内容
         with open(path, 'w', encoding='utf8') as file:
@@ -94,11 +59,25 @@ def load_files(rules, folder):
 
     print(f"新文件已下载并替换至：{target_directory}")
 
+RULES = {
+    # 你的规则定义
+}
+
+HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+TYPES = "Surge"
+file_paths = [TYPES + "/" + rule for rule in RULES.keys()]
+
+def download_and_save_file(url, target_path):
+    response = requests.get(url, headers=HEADER)
+    if response.status_code == 200:
+        with open(target_path, "wb") as f:
+            f.write(response.content)
+        time.sleep(1)
+
 if __name__ == '__main__':
     for folder, rules in RULES.items():
         load_files(rules, folder)
     for path in file_paths:
-        # 创建文件夹，如果文件夹不存在
         if not os.path.exists(path):
             os.makedirs(path)
             print(f"创建目录 {path} 成功")
@@ -106,7 +85,6 @@ if __name__ == '__main__':
         # 在这里添加自定义逻辑，比如其他文件操作或处理
         # ...
 
-        # 处理文件
         process_file(path)
 
         print(f"处理 {path} 完成")
