9af9656da88a48f8d4b29e3c588f463335d1ad77
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-26 08:36:38 +0800

    Update u.js

--- Scripts/u.js
+++ Scripts/u.js
@@ -1,1 +1,1 @@
-// 引用地址: https://gist.githubusercontent.com/ddgksf2013/9e0f6c7341beea09a31aa309d9d7f502/raw/buyitunes.js
+// 引用地址：https://gist.githubusercontent.com/ddgksf2013/9e0f6c7341beea09a31aa309d9d7f502/raw/buyitunes.js
