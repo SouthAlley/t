2f44fbf9a54e8909f24783e856a5fe736d81c5ab
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-26 22:58:01 +0800

    Update rule.py

--- Scripts/rule.py
+++ Scripts/rule.py
@@ -1,7 +1,7 @@
-import re
-import os  # Add this line
+import os
 import requests
-from git import Repo
+import re
+from concurrent.futures import ThreadPoolExecutor
 
 # 正则表达式替换规则
 replacements = [
@@ -16,21 +16,54 @@ replacements = [
     (r'//.*', ''),
 ]
 
-# 获取远程规则内容
-remote_url = "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliuxiuzheng.list"
-remote_content = requests.get(remote_url).text
+RULES = {
+    "CorrectionRule": {
+        "fenliuxiuzheng": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliuxiuzheng.list",
+        "MyCorrectionRule": "https://raw.githubusercontent.com/GiveYou32Likes/Profile/main/QuantumultX/Rule/CorrectionRule.list",
+    },
+    "RejectRule": {
+        "fenliu": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliu.list",
+        "MyBlockAds": "https://raw.githubusercontent.com/limbopro/Adblock4limbo/main/Surge/rule/Adblock4limbo_surge.list",
+        "MyRejectRule": "https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/Reject.list",
+        "Block": "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list"
+    },
+    "StreamingRule": {
+        "Netflix": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/PayPal.list",
+        "Disney": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/OpenAI.list",
+        # 根据需要添加更多的流媒体规则
+    }
+}
 
-# 应用替换规则
-for pattern, replacement in replacements:
-    remote_content = re.sub(pattern, replacement, remote_content)
+HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+TYPES = "Surge"
+file_paths = [TYPES + "/" + rule for rule in RULES.keys()]
 
-# 推送到本地仓库
-local_path = "t"
-repo = Repo(os.path.join("/github/workspace", local_path))
 
-with open(os.path.join(local_path, "fenliuxiuzheng_modified.list"), "w") as file:
-    file.write(remote_content)
+def download_and_save_file(url, target_path):
+    response = requests.get(url, headers=HEADER)
+    if response.status_code == 200:
+        with open(target_path, "wb") as f:
+            f.write(response.content)
+        print(f"下载成功: {target_path}")
 
-repo.index.add([os.path.join(local_path, "fenliuxiuzheng_modified.list")])
-repo.index.commit("Apply regex replacements to fenliuxiuzheng.list")
-repo.remote().push()
+
+def apply_replacements(line):
+    for pattern, replacement in replacements:
+        line = re.sub(pattern, replacement, line)
+    return line
+
+
+def load_files(rules, folder):
+    target_directory = os.path.join(TYPES, folder)
+    os.makedirs(target_directory, exist_ok=True)
+
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(download_and_save_file, rule_url, os.path.join(target_directory, f"{rule_name}.list"))
+                   for rule_name, rule_url in rules.items()]
+
+    print(f"新文件已下载至：{target_directory}")
+
+
+if __name__ == '__main__':
+    for folder, rules in RULES.items():
+        load_files(rules, folder)
