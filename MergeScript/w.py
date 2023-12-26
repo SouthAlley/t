18127682ae9a7aff3dbd9dbe8557eabd472940d0
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-26 23:41:39 +0800

    Create w.py

---
+++ MergeScript/w.py
@@ -0,0 +1,66 @@
+import os
+import re
+import time
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+# 正则表达式替换规则
+replacements = [
+    (r'\s+', ''),
+    (r',no-resolve', ''),
+    (r',(?:DIRECT$|direct$|REJECT$|reject$|PROXY$|proxy$)', ''),
+    (r'-suffix', '-SUFFIX'),
+    (r'-keyword', '-KEYWORD'),
+    (r'ip-cidr', 'IP-CIDR'),
+    (r'^(?:host|HOST)', 'DOMAIN'),
+    (r'IP6-CIDR', 'IP-CIDR6'),
+    (r'//.*', ''),
+]
+
+RULES = {
+    # ... (unchanged)
+}
+
+HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+TYPES = "Surge"
+file_paths = [TYPES + "/" + rule for rule in RULES.keys()]
+
+def download_and_save_file(url, target_path):
+    response = requests.get(url, headers=HEADER)
+    if response.status_code == 200:
+        with open(target_path, "wb") as f:
+            f.write(response.content)
+        time.sleep(1)
+
+def apply_replacements(line):
+    for pattern, replacement in replacements:
+        line = re.sub(pattern, replacement, line)
+    return line
+
+def load_files(rules, folder):
+    target_directory = os.path.join(TYPES, folder)
+    os.makedirs(target_directory, exist_ok=True)
+
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for rule_name, rule_url in rules.items():
+            target_path = os.path.join(target_directory, f"{rule_name}.list")
+            futures.append(executor.submit(download_and_save_file, rule_url, target_path))
+        
+        for future in futures:
+            future.result()
+        print(f"新文件已下载至：{target_directory}")
+
+if __name__ == '__main__':
+    for folder, rules in RULES.items():
+        load_files(rules, folder)
+    for path in file_paths:
+        # 创建文件夹，如果文件夹不存在
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print(f"创建目录 {path} 成功")
+            
+        # 在这里添加自定义逻辑，比如其他文件操作或处理
+        # ...
+
+        print(f"处理 {path} 完成")
