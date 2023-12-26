43f844b255c642a280239127b795288319c74133
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-26 23:55:04 +0800

    Update w.py

--- MergeScript/w.py
+++ MergeScript/w.py
@@ -18,7 +18,33 @@ replacements = [
 ]
 
 RULES = {
-    # ... (unchanged)
+    "CorrectionRule": {
+        "fenliuxiuzheng": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliuxiuzheng.list",
+        "MyCorrectionRule": "https://raw.githubusercontent.com/GiveYou32Likes/Profile/main/QuantumultX/Rule/CorrectionRule.list",
+    },
+    "RejectRule": {
+        "fenliu": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliu.list",
+        "MyBlockAds": "https://raw.githubusercontent.com/limbopro/Adblock4limbo/main/Surge/rule/Adblock4limbo_surge.list",
+        "MyRejectRule": "https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/Reject.list",
+        "Block": "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list"
+    },
+    "StreamingRule": {
+        "Netflix": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/PayPal.list",
+        "Disney": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/OpenAI.list",
+        "YouTube": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/TikTok.list",
+        "Spotify": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/ProxyMedia.list",
+        "AppleTV": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Twitter.list",
+        "Netfli": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Telegram.list",
+        "Disne": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Game.list",
+        "YouTub": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Microsoft.list",
+        "Spotif": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Google.list",
+        "AppleT": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/AppleProxy.list",
+        "Spoti": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Apple.list",
+        "Apple": "https://raw.githubusercontent.com/Jard1n/VPN_Tool/main/Surge/Rule/Proxy.list",
+        "Appl": "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/BilibiliHMT.list",
+        "App": "https://raw.githubusercontent.com/ConnersHua/RuleGo/master/Surge/Ruleset/Extra/Streaming/Video/bilibili.list",
+        
+    }
 }
 
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
@@ -49,7 +75,20 @@ def load_files(rules, folder):
         
         for future in futures:
             future.result()
-        print(f"新文件已下载至：{target_directory}")
+    
+    # 读取并应用替换规则
+    for path in [os.path.join(target_directory, f"{rule_name}.list") for rule_name in rules.keys()]:
+        with open(path, 'r', encoding='utf8') as file:
+            lines = [line.strip() for line in file.readlines()]
+
+        # 应用替换规则
+        modified_lines = [apply_replacements(line) for line in lines]
+
+        # 保存修改后的内容
+        with open(path, 'w', encoding='utf8') as file:
+            file.write('\n'.join(modified_lines))
+
+    print(f"新文件已下载并替换至：{target_directory}")
 
 if __name__ == '__main__':
     for folder, rules in RULES.items():
@@ -64,3 +103,4 @@ if __name__ == '__main__':
         # ...
 
         print(f"处理 {path} 完成")
+
