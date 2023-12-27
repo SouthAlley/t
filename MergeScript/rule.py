97ba51b50193cdcf3de8d7a4e1f37db5ecee4f46
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2023-12-27 10:48:05 +0800

    Create rule.py

---
+++ MergeScript/rule.py
@@ -0,0 +1,68 @@
+import os
+import re
+import requests
+
+# 正则表达式替换规则
+replacements = [
+    (r', ', ','),
+    (r'([^,]*,[^,]*),.*', r'\1'),
+    (r'ip-cidr,', 'IP-CIDR,'),
+    (r'(?i)host,', 'DOMAIN,'),
+    (r'(?i)host-wildcard,[^,]*', ''),
+    (r'(?i)ip6-cidr,', 'IP-CIDR6,'),
+    (r'(?i)host-keyword,', 'DOMAIN-KEYWORD,'),
+    (r'(?i)host-suffix,', 'DOMAIN-SUFFIX,'),
+    (r'(IP-CIDR[6]{0,1},[^,]*)', r'\1,no-resolve'),
+    (r'//.*', ''),
+]
+
+RULES = {
+    "fenliu": "https://raw.githubusercontent.com/fmz200/wool_scripts/main/QuantumultX/filter/fenliu.list",
+    "MyBlockAds": "https://raw.githubusercontent.com/limbopro/Adblock4limbo/main/Surge/rule/Adblock4limbo_surge.list",
+    "MyRejectRule": "https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/Reject.list",
+    "Block": "https://raw.githubusercontent.com/SouthAlley/z/main/Surge/G.list",
+}
+
+HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+TYPES = "Surge"
+
+def download_and_save_file(url, target_path):
+    try:
+        response = requests.get(url, headers=HEADER)
+        response.raise_for_status()  # 抛出异常如果请求失败
+        with open(target_path, "wb") as f:
+            f.write(response.content)
+        print(f"文件下载完成：{target_path}")
+    except requests.exceptions.RequestException as e:
+        print(f"下载文件失败：{url}, 错误：{e}")
+    except Exception as e:
+        print(f"下载文件时出现未知错误：{url}, 错误：{e}")
+
+def apply_replacements(lines):
+    return [apply_replacements_to_line(line) for line in lines]
+
+def apply_replacements_to_line(line):
+    for pattern, replacement in replacements:
+        line = re.sub(pattern, replacement, line)
+    return line
+
+if __name__ == '__main__':
+    for rule_name, rule_url in RULES.items():
+        try:
+            response = requests.get(rule_url, headers=HEADER)
+            response.raise_for_status()  # 抛出异常如果请求失败
+            lines = response.text.splitlines()
+
+            # 应用替换规则
+            modified_lines = apply_replacements(lines)
+
+            # 保存修改后的内容
+            target_path = os.path.join(TYPES, f"{rule_name}.list")
+            with open(target_path, 'w', encoding='utf8') as file:
+                file.write('\n'.join(modified_lines))
+
+            print(f"文件处理完成：{target_path}")
+        except requests.exceptions.RequestException as e:
+            print(f"处理文件时出现错误：{rule_url}, 错误：{e}")
+        except Exception as e:
+            print(f"处理文件时出现未知错误：{rule_url}, 错误：{e}")
