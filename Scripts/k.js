417aa3490d72a65f1084f9cf182454cd8f4d66b4
SouthAlley <84231965+SouthAlley@users.noreply.github.com>
2024-01-02 13:51:32 +0800

    Update k.js

--- Scripts/k.js
+++ Scripts/k.js
@@ -1,1 +1,1 @@
-https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
+// 引用地址：https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
